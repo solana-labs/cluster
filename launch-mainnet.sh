f047993c43da07f8c724d158ecb9c086e66d08b3
Michael Vines <mvines@gmail.com>
2019-12-02 23:09:29 -0700

    Add warehouse node

--- launch-mainnet.sh
+++ launch-mainnet.sh
@@ -10,7 +10,7 @@ set -e
 cd "$(dirname "$0")"
 source env.sh
 
-RELEASE_CHANNEL_OR_TAG=0.21.0
+RELEASE_CHANNEL_OR_TAG=beta
 
 usage() {
   exitcode=0
@@ -44,15 +44,21 @@ done
 ENTRYPOINT_INSTANCE=${INSTANCE_PREFIX}entrypoint-mainnet-solana-com
 BOOTSTRAP_LEADER_INSTANCE=${INSTANCE_PREFIX}bootstrap-leader-mainnet-solana-com
 API_INSTANCE=${INSTANCE_PREFIX}api-mainnet-solana-com
+WAREHOUSE_INSTANCE=${INSTANCE_PREFIX}warehouse-mainnet-solana-com
 
-INSTANCES="$ENTRYPOINT_INSTANCE $BOOTSTRAP_LEADER_INSTANCE $API_INSTANCE"
+INSTANCES="
+  $ENTRYPOINT_INSTANCE
+  $BOOTSTRAP_LEADER_INSTANCE
+  $API_INSTANCE
+  $WAREHOUSE_INSTANCE
+"
 
 if [[ $(basename "$0" .sh) = delete-mainnet ]]; then
   if [[ -n $PRODUCTION ]]; then
     echo "Attempting to recover TLS certificate before deleting instances"
     (
       set -x
-      gcloud --project $PROJECT compute scp --zone $ZONE $API_INSTANCE:/letsencrypt.tgz .
+      gcloud --project "$PROJECT" compute scp --zone "$ZONE" "$API_INSTANCE":/letsencrypt.tgz .
     ) || true
     if [[ -f letsencrypt.tgz ]]; then
       echo "Warning: ensure you don't delete letsencrypt.tgz"
@@ -62,7 +68,7 @@ if [[ $(basename "$0" .sh) = delete-mainnet ]]; then
   (
     set -x
     # shellcheck disable=SC2086 # Don't want to double quote INSTANCES
-    gcloud --project $PROJECT compute instances delete $INSTANCES --zone $ZONE --quiet
+    gcloud --project "$PROJECT" compute instances delete $INSTANCES --zone "$ZONE" --quiet
   )
   exit 0
 fi
@@ -78,39 +84,55 @@ if [[ ! -d ledger ]]; then
   exit 1
 fi
 
-
-GENESIS_HASH="$(RUST_LOG=none solana-ledger-tool print-genesis-hash --ledger ledger)"
-if [[ -n $PRODUCTION ]]; then
-  SOLANA_METRICS_CONFIG="host=https://metrics.solana.com:8086,db=mainnet,u=mainnet_write,p=2aQdShmtsPSAgABLQiK2FpSCJGLtG8h3vMEVz1jE7Smf"
-elif [[ -z $SOLANA_METRICS_CONFIG ]]; then
-  echo Note: SOLANA_METRICS_CONFIG is not configured
-fi
-
-(
-  echo EXPECTED_GENESIS_HASH="$GENESIS_HASH"
-  if [[ -n $SOLANA_METRICS_CONFIG ]]; then
-    echo SOLANA_METRICS_CONFIG="$SOLANA_METRICS_CONFIG"
-  fi
-) | tee service-env.sh
-
-
 for instance in $INSTANCES; do
   echo "Checking that \"$instance\" does not exist"
-  status=$(gcloud --project $PROJECT compute instances list --filter name="$instance" --format 'value(status)')
+  status=$(gcloud --project "$PROJECT" compute instances list --filter name="$instance" --format 'value(status)')
   if [[ -n $status ]]; then
     echo "Error: $instance already exists (status=$status)"
     exit 1
   fi
 done
 
+GENESIS_HASH="$(RUST_LOG=none solana-ledger-tool print-genesis-hash --ledger ledger)"
+STORAGE_BUCKET=${INSTANCE_PREFIX}solana-ledger
+
+if [[ -n $PRODUCTION ]]; then
+  SOLANA_METRICS_CONFIG="host=https://metrics.solana.com:8086,db=mainnet,u=mainnet_write,p=2aQdShmtsPSAgABLQiK2FpSCJGLtG8h3vMEVz1jE7Smf"
+
+  # Create the production bucket if it doesn't already exist but do not remove old
+  # data, if any, to avoid accidental data loss.
+  gsutil mb -p "$PROJECT" -l "$REGION" -b on gs://$"STORAGE_BUCKET" || true
+elif [[ -z $SOLANA_METRICS_CONFIG ]]; then
+  echo Note: SOLANA_METRICS_CONFIG is not configured
+
+  # Re-create the dev bucket on each launch
+  gsutil rm -r gs://"$STORAGE_BUCKET" || true
+  gsutil mb -p "$PROJECT" -l "$REGION" -b on gs://"$STORAGE_BUCKET"
+fi
+
+(
+  set -x
+  gsutil -m cp -r ledger/genesis.tar.bz2 gs://"$STORAGE_BUCKET"
+)
+
+(
+  echo EXPECTED_GENESIS_HASH="$GENESIS_HASH"
+  if [[ -n $SOLANA_METRICS_CONFIG ]]; then
+    echo SOLANA_METRICS_CONFIG="$SOLANA_METRICS_CONFIG"
+  fi
+  echo STORAGE_BUCKET="$STORAGE_BUCKET"
+  echo PATH=/home/solanad/.local/share/solana/install/active_release/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+) | tee service-env.sh
+
+
 echo ==========================================================
 echo "Creating $ENTRYPOINT_INSTANCE"
 echo ==========================================================
 (
   set -x
-  gcloud --project $PROJECT compute instances create \
+  gcloud --project "$PROJECT" compute instances create \
     "$ENTRYPOINT_INSTANCE" \
-    --zone $ZONE \
+    --zone "$ZONE" \
     --machine-type n1-standard-1 \
     --boot-disk-size=200GB \
     --tags solana-validator-minimal \
@@ -123,9 +145,9 @@ echo "Creating $API_INSTANCE"
 echo ==========================================================
 (
   set -x
-  gcloud --project $PROJECT compute instances create \
+  gcloud --project "$PROJECT" compute instances create \
     "$API_INSTANCE" \
-    --zone $ZONE \
+    --zone "$ZONE" \
     --machine-type n1-standard-8 \
     --boot-disk-size=2TB \
     --tags solana-validator-minimal,solana-validator-rpc \
@@ -138,33 +160,49 @@ echo "Creating $BOOTSTRAP_LEADER_INSTANCE"
 echo ==========================================================
 (
   set -x
-  gcloud --project $PROJECT compute instances create \
+  gcloud --project "$PROJECT" compute instances create \
     "$BOOTSTRAP_LEADER_INSTANCE" \
-    --zone $ZONE \
+    --zone "$ZONE" \
     --machine-type n1-standard-8 \
     --boot-disk-size=2TB \
     --tags solana-validator-minimal,solana-validator-rpc \
     --image ubuntu-minimal-1804-bionic-v20191113 --image-project ubuntu-os-cloud
 )
 
+echo ==========================================================
+echo "Creating $WAREHOUSE_INSTANCE"
+echo ==========================================================
+(
+  set -x
+  gcloud --project "$PROJECT" compute instances create \
+    "$WAREHOUSE_INSTANCE" \
+    --zone "$ZONE" \
+    --machine-type n1-standard-8 \
+    --boot-disk-size=2TB \
+    --tags solana-validator-minimal,solana-validator-rpc \
+    --image ubuntu-minimal-1804-bionic-v20191113 --image-project ubuntu-os-cloud \
+    --scopes=storage-rw
+)
+
 ENTRYPOINT=mainnet.solana.com
 RPC=api.mainnet.solana.com
 
 if [[ -n $INSTANCE_PREFIX ]]; then
-  ENTRYPOINT=$(gcloud --project $PROJECT compute instances list \
+  ENTRYPOINT=$(gcloud --project "$PROJECT" compute instances list \
       --filter name="$ENTRYPOINT_INSTANCE" --format 'value(networkInterfaces[0].accessConfigs[0].natIP)')
-  RPC=$(gcloud --project $PROJECT compute instances list \
+  RPC=$(gcloud --project "$PROJECT" compute instances list \
       --filter name="$API_INSTANCE" --format 'value(networkInterfaces[0].accessConfigs[0].natIP)')
 fi
-echo "ENTRYPOINT=$ENTRYPOINT" >> service-env.sh
 RPC_URL="http://$RPC/"
+echo "RPC_URL=$RPC_URL" >> service-env.sh
+echo "ENTRYPOINT=$ENTRYPOINT" >> service-env.sh
 
 echo ==========================================================
 echo Waiting for instances to boot
 echo ==========================================================
 # shellcheck disable=SC2068 # Don't want to double quote INSTANCES
 for instance in ${INSTANCES[@]}; do
-  while ! gcloud --project $PROJECT compute ssh --zone $ZONE "$instance" -- true; do
+  while ! gcloud --project "$PROJECT" compute ssh --zone "$ZONE" "$instance" -- true; do
     echo "Waiting for \"$instance\" to boot"
     sleep 5s
   done
@@ -174,7 +212,7 @@ echo ==========================================================
 echo "Transferring files to $ENTRYPOINT_INSTANCE"
 echo ==========================================================
 (
-  gcloud --project $PROJECT compute scp --zone $ZONE --recurse \
+  gcloud --project "$PROJECT" compute scp --zone "$ZONE" --recurse \
     remote-machine-setup.sh \
     service-env.sh \
     entrypoint.service \
@@ -186,7 +224,7 @@ echo "Transferring files to $BOOTSTRAP_LEADER_INSTANCE"
 echo ==========================================================
 (
   set -x
-  gcloud --project $PROJECT compute scp --zone $ZONE --recurse \
+  gcloud --project "$PROJECT" compute scp --zone "$ZONE" --recurse \
     bootstrap-leader-identity.json \
     bootstrap-leader-stake-account.json \
     bootstrap-leader-vote-account.json \
@@ -198,11 +236,25 @@ echo ==========================================================
 )
 
 echo ==========================================================
+echo "Transferring files to $WAREHOUSE_INSTANCE"
+echo ==========================================================
+(
+  set -x
+  gcloud --project "$PROJECT" compute scp --zone "$ZONE" --recurse \
+    remote-machine-setup.sh \
+    ledger \
+    service-env.sh \
+    warehouse.service \
+    warehouse-node.sh \
+    "$WAREHOUSE_INSTANCE":
+)
+
+echo ==========================================================
 echo "Transferring files to $API_INSTANCE"
 echo ==========================================================
 (
   set -x
-  gcloud --project $PROJECT compute scp --zone $ZONE --recurse \
+  gcloud --project "$PROJECT" compute scp --zone "$ZONE" --recurse \
     remote-machine-setup.sh \
     ledger \
     service-env.sh \
@@ -212,8 +264,8 @@ echo ==========================================================
 if [[ -n $PRODUCTION && -f letsencrypt.tgz ]]; then
   (
     set -x
-    gcloud --project $PROJECT compute scp --zone $ZONE letsencrypt.tgz "$API_INSTANCE":~/letsencrypt.tgz
-    gcloud --project $PROJECT compute ssh --zone $ZONE "$API_INSTANCE" -- sudo mv letsencrypt.tgz /
+    gcloud --project "$PROJECT" compute scp --zone "$ZONE" letsencrypt.tgz "$API_INSTANCE":~/letsencrypt.tgz
+    gcloud --project "$PROJECT" compute ssh --zone "$ZONE" "$API_INSTANCE" -- sudo mv letsencrypt.tgz /
   )
 fi
 
@@ -224,15 +276,31 @@ for instance in $INSTANCES; do
   echo ==========================================================
   (
     nodeType=
-    if [[ $instance = "$API_INSTANCE" ]]; then
-        nodeType=api
-    fi
+    case $instance in
+    $API_INSTANCE)
+      nodeType=api
+      ;;
+    $WAREHOUSE_INSTANCE)
+      nodeType=warehouse
+      ;;
+    $ENTRYPOINT_INSTANCE)
+      nodeType=entrypoint
+      ;;
+    $BOOTSTRAP_LEADER_INSTANCE)
+      nodeType=bootstrap
+      ;;
+    *)
+      echo "Error: Unknown instance type: $instance"
+      exit 1
+      ;;
+    esac
+
     if [[ -n $PRODUCTION ]]; then
       nodeType="${nodeType}production"
     fi
 
     set -x
-    gcloud --project $PROJECT compute ssh --zone $ZONE "$instance" -- \
+    gcloud --project "$PROJECT" compute ssh --zone "$ZONE" "$instance" -- \
       bash remote-machine-setup.sh "$RELEASE_CHANNEL_OR_TAG" "$nodeType"
   )
 done
