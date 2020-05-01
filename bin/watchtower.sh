2d7a7408b11271931c897e231bdc7144f459d6af
Michael Vines <mvines@gmail.com>
2020-05-01 14:37:11 -0700

    Add --notify-on-transactions support

--- bin/watchtower.sh
+++ bin/watchtower.sh
@@ -4,13 +4,18 @@ set -ex
 #shellcheck source=/dev/null
 . ~/service-env.sh
 
-validators=()
-for tv in "${TRUSTED_VALIDATOR_PUBKEYS[@]}"; do
-  validators+=(--validator-identity "$tv")
-done
-
-exec solana-watchtower \
+args=(
   --url "$RPC_URL" \
   --monitor-active-stake \
   --no-duplicate-notifications \
-  "${validators[@]}" \
+)
+
+for tv in "${TRUSTED_VALIDATOR_PUBKEYS[@]}"; do
+  args+=(--validator-identity "$tv")
+done
+
+if [[ -n TRANSACTION_NOTIFIER_SLACK_WEBHOOK ]]; then
+  args+=(--notify-on-transactions)
+fi
+
+exec solana-watchtower "${args[@]}"
