685361c81d51a9e1f889f5ad428ab673940072f2
Trent Nelson <trent@solana.com>
2020-08-10 17:04:31 -0600

    Fix keypair name discrepency between check and create

--- keygen.sh
+++ keygen.sh
@@ -11,11 +11,11 @@ keygen() {
 
   solana-keygen --version
   for zone in "${VALIDATOR_ZONES[@]}"; do
-    test -f "$CLUSTER"/validator-"$zone"-identity.json ||
+    test -f "$CLUSTER"/validator-identity-"$zone".json ||
       (set -x; solana-keygen $cmd --outfile "$CLUSTER"/validator-identity-"$zone".json)
-    test -f "$CLUSTER"/validator-"$zone"-vote-account.json ||
+    test -f "$CLUSTER"/validator-vote-account-"$zone".json ||
       (set -x; solana-keygen $cmd --outfile "$CLUSTER"/validator-vote-account-"$zone".json)
-    test -f "$CLUSTER"/validator-"$zone"-stake-account.json ||
+    test -f "$CLUSTER"/validator-stake-account-"$zone".json ||
       (set -x; solana-keygen $cmd --outfile "$CLUSTER"/validator-stake-account-"$zone".json)
   done
 
@@ -28,7 +28,7 @@ keygen() {
     (set -x; solana-keygen $cmd --outfile "$CLUSTER"/api-identity.json)
 
   for zone in "${WAREHOUSE_ZONES[@]}"; do
-    test -f "$CLUSTER"/warehouse-"$zone"-identity.json ||
+    test -f "$CLUSTER"/warehouse-identity-"$zone".json ||
       (set -x; solana-keygen $cmd --outfile "$CLUSTER"/warehouse-identity-"$zone".json)
   done
 }
