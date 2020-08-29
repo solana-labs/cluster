b6d55cb59e1b3f13ffecd0fd4bead67dbeef49f4
Michael Vines <mvines@gmail.com>
2020-08-28 20:45:39 -0700

    Regen api key on hostname change

--- bin/check-hostname.sh
+++ bin/check-hostname.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 if [ ! -r ~/.hostname ] || ! diff /etc/hostname ~/.hostname; then
-  echo "Error: hostname has changed. To continue, run: cp /etc/hostname ~/.hostname"
-  exit 1
+  if [ -f ~/api-identity.json ]; then
+    echo "Hostname has changed, regenerating keys"
+    solana-keygen new --force --outfile ~/api-identity.json --no-passphrase
+    cp /etc/hostname ~/.hostname
+  else
+    echo "Error: hostname has changed. To continue, run: cp /etc/hostname ~/.hostname"
+    exit 1
+  fi
 fi
