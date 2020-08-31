2a80490ae7a714fbd952e14c51bd7209d4af3ee1
Michael Vines <mvines@gmail.com>
2020-08-30 17:41:17 -0700

    Limit faucet

--- bin/run-faucet.sh
+++ bin/run-faucet.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -ex
 
 . ~/service-env.sh
-exec solana-faucet --keypair ~/faucet.json
+exec solana-faucet \
+  --keypair ~/faucet.json \
+  --per-request-cap 10 \
+  --per-time-cap 10 \
+  --slice 10
