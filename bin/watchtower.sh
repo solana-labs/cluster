83c1fff1a0769948f1da4f1be650350dba26c815
Michael Vines <mvines@gmail.com>
2020-11-28 17:00:51 -0800

    Remove --notify-on-transaction support

--- bin/watchtower.sh
+++ bin/watchtower.sh
@@ -14,8 +14,4 @@ for tv in "${TRUSTED_VALIDATOR_PUBKEYS[@]}"; do
   args+=(--validator-identity "$tv")
 done
 
-if [[ -n $TRANSACTION_NOTIFIER_SLACK_WEBHOOK ]]; then
-  args+=(--notify-on-transactions)
-fi
-
 exec solana-watchtower "${args[@]}"
