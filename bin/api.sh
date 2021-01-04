4e1483b0e9e857d9712b46183eaef18bbd21155d
Tyera Eulberg <teulberg@gmail.com>
2021-01-04 12:54:56 -0700

    Add account-index optional arg (#33)
    * Add account-index optional arg
    
    * Make multiple indexes more explicit

--- bin/api.sh
+++ bin/api.sh
@@ -79,6 +79,10 @@ if [[ -n "$RPC_HEALTH_CHECK_SLOT_DISTANCE" ]]; then
   args+=(--health-check-slot-distance "$RPC_HEALTH_CHECK_SLOT_DISTANCE")
 fi
 
+for index in "${ACCOUNT_INDEXES[@]}"; do
+  args+=(--account-index "$index")
+done
+
 if [[ -r ~/api-vote-account.json ]]; then
   args+=(--vote-account ~/api-vote-account.json)
 else
