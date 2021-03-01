ce10a54e1e1a01ac13c20ee53a2cd72fc99438e3
joeaba <77398477+joeaba@users.noreply.github.com>
2021-03-02 04:38:48 +0530

    Add files via upload

---
+++ safety.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+currenttime=`date +"%d-%b-%Y %H:%M:%S"`
+logfile=`date +"%d-%b-%Y"`_log.log
+echo "================= $currenttime"  >> $logfile
+RMLEDGER=3000
+FSNAPSHOT=1000
+CLUSTERSLOT=$(solana slot -u http://10.142.0.4:8899)
+NODESLOT=$(solana slot -u http://127.0.0.1:8899)
+DIFFSLOT=$(($CLUSTERSLOT-$NODESLOT))
+echo "Cluster Slot:" $CLUSTERSLOT "Current Slot:" $NODESLOT "Difference in Slots:" $DIFFSLOT >> $logfile
+if [ $DIFFSLOT -gt $RMLEDGER ]; then
+        cd ~
+        ./stop
+        rm -rf ledger/
+        ./restart
+        echo "Node was:" $DIFFSLOT " behind, Services has been stopped, ledger deleted and service restarted" >> $logfile
+elif [ $DIFFSLOT -gt $FSNAPSHOT ]; then
+        cd ~
+        ./stop
+        ./fetch-snapshot.sh bv1
+        ./restart
+        echo "Node was:" $DIFFSLOT " behind, Services has been stopped, snapshot restored from bv1 and service restarted" >> $logfile
+else
+        echo "Node was:" $DIFFSLOT " behind, so no Operation performed" >> $logfile
+fi
