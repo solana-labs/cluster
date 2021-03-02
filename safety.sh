0e38dd3a35d587abac1079ee92fe9990e0794de6
joeaba <77398477+joeaba@users.noreply.github.com>
2021-03-02 00:17:09 -0500

    Update safety.sh

--- safety.sh
+++ safety.sh
@@ -2,8 +2,7 @@
 currenttime=`date +"%d-%b-%Y %H:%M:%S"`
 logfile=`date +"%d-%b-%Y"`_log.log
 echo "================= $currenttime"  >> $logfile
-RMLEDGER=3000
-FSNAPSHOT=1000
+RMLEDGER=1500
 CLUSTERSLOT=$(solana slot -u http://10.142.0.4:8899)
 NODESLOT=$(solana slot -u http://127.0.0.1:8899)
 DIFFSLOT=$(($CLUSTERSLOT-$NODESLOT))
@@ -14,12 +13,6 @@ if [[ $DIFFSLOT -gt $RMLEDGER ]]; then
         rm -rf ledger/
         ./restart
         echo "Node was:" $DIFFSLOT " slots behind, Services has been stopped, ledger deleted and service restarted" >> $logfile
-elif [[ $DIFFSLOT -gt $FSNAPSHOT ]]; then
-        cd ~
-        ./stop
-        ./fetch-snapshot.sh bv1
-        ./restart
-        echo "Node was:" $DIFFSLOT " slots behind, Services has been stopped, snapshot restored from bv1 and service restarted" >> $logfile
 else
         echo "Node was:" $DIFFSLOT " slots behind, so no operation performed" >> $logfile
 fi
